fb4fa74b2d61e80f2a64e13bb6f83cef4a80d23e
Rico Sassen <sar@neusta.de>
2021-04-15 14:28:05 +0200

    passe branch auf develop an. mob-session nun in feature/mob-session

--- mob.bat
+++ mob.bat
@@ -4,18 +4,19 @@ IF "%~1"=="" GOTO end
 IF "%~1"=="start" GOTO start
 IF "%~1"=="go" GOTO go
 IF "%~1"=="next" GOTO next
-IF "%~1"=="stop" GOTO stop
 IF "%~1"=="clear" GOTO clear
 SHIFT
 GOTO parse
 
 :start
 ECHO Starte Session..
-
-IF "%~2"=="" (git checkout master) else (git checkout "%~2")
+IF "%~2"=="" (git checkout develop) else (git checkout "%~2")
+ECHO Räume Session auf
+git checkout develop
+git branch -D feature/mob-session
 git pull
-git checkout -b features/mob-session
-git push -u origin features/mob-session
+git checkout -b feature/mob-session
+git push -u origin feature/mob-session
 ECHO ..Session begonnen
 GOTO end
 
@@ -29,29 +30,15 @@ GOTO end
 :go
 ECHO Übernehme das Steuer
 git fetch
-git checkout "features/mob-session"
+git checkout feature/mob-session
 git pull
 GOTO end
-
-:stop
-ECHO Beende Session..
-IF "%~2"=="" GOTO branchfehlt
-GOTO squashmaster
 
 :clear
 ECHO Räume Session auf
-git checkout master
-git branch -D features/mob-session
+git checkout develop
+git branch -D feature/mob-session
 git pull
-GOTO end
-
-:squashmaster
-git checkout master
-git pull
-git merge --squash features/mob-session
-git commit -m"%~2"
-git push
-ECHO ..Session beendet
 GOTO end
 
 :branchfehlt
